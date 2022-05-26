ccab010683030e5ecc4f61476e65716e90bee821
Bartkooo <bartosz.plochocki@gmail.com>
2022-05-26 21:53:58 +0200

    days to holidays - working

--- src/components/holidays.js
+++ src/components/holidays.js
@@ -1,23 +1,30 @@
-import moment from "moment";
+import moment from 'moment';
 
 function Holidays() {
   const date = new Date();
-  const day = date.getDate();
-  const month = date.getMonth() + 1;
-  const year = date.getFullYear();
+  const month = date.getMonth();
 
-  const now = moment([year, month, day]);
-  const firstDayOfHolidays = moment([year, 6, 23]);
-  
-  const daysToHolidays = firstDayOfHolidays.diff(now, 'days')+1
+  function lastFriday() {
+    let sub = 0;
+    const lastDay = moment().month('june').endOf('month');
+    if (lastDay.day() >= 5) sub = lastDay.day() - 5;
+    else sub = lastDay.day() + 2;
+    return lastDay.subtract(sub, 'days');
+  }
 
-  return(
+  const now = moment();
+  const firstDayOfHolidays = lastFriday();
+  let daysToHolidays = firstDayOfHolidays.diff(now, 'days') - 1;
+  if (daysToHolidays < 0) daysToHolidays = 365 + daysToHolidays;
+
+  return (
     <div className="holidays">
-      {(month === 7 || month === 8) ? <div>Wakacje!</div> :
-       <div>Szkoła...</div>}
-      <div>{daysToHolidays}</div>
+      {month === 7 || month === 8 ? <div>Wakacje!</div> : <div>Szkoła...</div>}
+      <div>
+        <h1>{daysToHolidays}</h1> dni do wakacji
+      </div>
     </div>
-  )
+  );
 }
 
-export default Holidays
+export default Holidays;
