b55f636174e5b4e40c8d6543a87098ce77d480ee
Bartkooo <bartosz.plochocki@gmail.com>
2022-05-26 23:02:35 +0200

    refresh time

--- src/components/date.js
+++ src/components/date.js
@@ -1,11 +1,24 @@
+import React, { useState, useEffect } from "react";
 
 function Data () {
+  const monthNames = ["styczeń","luty","marzec","kwiecień","maj","czerwiec","lipiec","sierpień","wrzesień","październik","listopad","grudzień"]
   const today = new Date();
-  const monthNames = ["styczeń","luteń","marzecień","kwiecień","majecień","czerwiecień","lipiecień","sierpiecień","wrzesiecień","paździercień","listopacień","grudziecień"]
-  const time = today.getHours() + ":" + today.getMinutes(); 
+  const currentTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
   const date = today.getDate() + " " + monthNames[today.getMonth()] + " " + today.getFullYear();
+
+  const [time, setTime] = useState(currentTime)
+
+  function refreshTime() {
+    const now = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    setTime(now)
+  }
+
+  useEffect(() => {
+    setInterval(refreshTime, 1000)
+  }, [time]);
+
   return(
-    <div className="date">{time}<br />{date}</div>
+    <div className="date">{currentTime}<br />{date}</div>
   )
 }
 
