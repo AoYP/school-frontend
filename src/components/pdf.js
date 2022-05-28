c782add0f020041b8b2d276774acdade10f0f7b5
Bartkooo <bartosz.plochocki@gmail.com>
2022-05-28 17:02:18 +0200

    implement pdf

--- src/components/pdf.js
+++ src/components/pdf.js
@@ -1,5 +1,26 @@
+import React, { useState, useEffect } from 'react'
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack'
+
 function PDF() {
-  return <div className="PDF">PDF</div>
+  const [pageNumber, setPageNumber] = useState(1)
+
+  function changePage() {
+    setPageNumber(prevPage => prevPage + 1)
+  }
+
+  useEffect(() => {
+    setInterval(changePage, 3000)
+  }, [pageNumber])
+
+  return (
+    <div className="PDF">
+      <Document
+        file="Konfiguraca przełacznika Planet - VLAN.pdf"
+      >
+        <Page pageNumber={pageNumber} />
+      </Document>
+    </div>
+  )
 }
 
 export default PDF
