ac7da280e313044901439c901cc0be8d655195da
Bartkooo <bartosz.plochocki@gmail.com>
2022-06-11 11:51:42 +0200

    little update

--- src/components/pdf.js
+++ src/components/pdf.js
@@ -1,24 +1,41 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack'
 
 function PDF() {
+  const [numPages, setNumPages] = useState(null)
   const [pageNumber, setPageNumber] = useState(1)
 
-  function changePage() {
-    setPageNumber(prevPage => prevPage + 1)
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages)
+    setPageNumber(1)
   }
 
-  useEffect(() => {
-    setInterval(changePage, 3000)
-  }, [pageNumber])
+  function changePage() {
+    if(pageNumber < numPages) {
+      setPageNumber(pageNumber + 1)
+    } else if (pageNumber == numPages) {
+      setPageNumber(1)
+    }
+  }
+
+
+  const interval = setInterval(() => {
+    clearInterval(interval)
+    changePage()
+  }, 3000)
+
 
   return (
     <div className="PDF">
       <Document
         file="Konfiguraca przełacznika Planet - VLAN.pdf"
+        onLoadSuccess={onDocumentLoadSuccess}
       >
-        <Page pageNumber={pageNumber} />
+        <Page pageNumber={pageNumber} height="400" />
       </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
     </div>
   )
 }
